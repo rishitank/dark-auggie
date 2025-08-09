2f73d0467d71c364cfc11303ca52ee124764569c
Rishi Tank <tank.rishi@gmail.com>
2025-08-09 02:27:50 +0100

    chore(release): semantic-release config + dry-run workflow
    Signed-off-by: Rishi Tank <tank.rishi@gmail.com>

---
+++ .releaserc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  // Release from either main or master; add prerelease branches later if needed
+  branches: ["main", "master"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+        changelogTitle: "# Changelog",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ].concat(
+    process.env.PUBLISH_NPM === "true" && process.env.NPM_TOKEN
+      ? [["@semantic-release/npm", { npmPublish: true }]]
+      : []
+  ),
+};
+
